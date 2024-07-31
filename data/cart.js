03fe349e2f00615cebf14343d3bed0340230f50b
Pieta02 <piotr.szymon.lewinski@gmail.com>
2024-07-31 18:23:57 +0200

    Learn primises and  convert code into promises

--- data/cart.js
+++ data/cart.js
@@ -84,3 +84,16 @@ export function updateQuantity(productId, newQuantity) {
 
   saveToStorage();
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+
+    fun();
+  });
+
+  xhr.open('Get', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
